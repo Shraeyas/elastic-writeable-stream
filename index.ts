a7da5fba17158c244b3874de6193bad27d79b37c
Shreyas <shreyassingh.official@gmail.com>
2023-12-04 21:41:20 +0530

    add multiple client connect options

--- index.ts
+++ index.ts
@@ -1,26 +1,35 @@
 import stream, { WritableOptions } from 'stream';
-import * as elasticsearch from 'elasticsearch';
+import { Client } from '@elastic/elasticsearch'
 import { BufferEncoding } from 'typescript';
 
 interface ElasticsearchConfig {
     host: string;
     index: string;
     type: string;
+    auth: any;
+    cloud: any;
+    node: any;
 }
 
 class ElasticsearchWritableStream extends stream.Writable {
     private config: ElasticsearchConfig;
-    private client: elasticsearch.Client;
+    private client: Client;
 
     constructor(config: ElasticsearchConfig, options?: WritableOptions) {
         super(options);
         this.config = config;
-        this.client = new elasticsearch.Client({
-            host: this.config.host,
+        this.client = new Client({
+            cloud: this.config.cloud,
+            auth: this.config.auth,
+            node: this.config.node,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
         });
     }
 
-    _destroy(): void {
+    _destroy(): Promise<void> {
         return this.client.close();
     }
 
@@ -32,9 +41,8 @@ class ElasticsearchWritableStream extends stream.Writable {
         try {
             await this.client.index({
                 index: this.config.index,
-                type: this.config.type,
-                body: body.toString(),
-            });
+                body: body
+            })
             next();
         } catch (err) {
             next(err);
@@ -43,7 +51,7 @@ class ElasticsearchWritableStream extends stream.Writable {
 
     async _writev(
         chunks: Array<{ chunk: any; encoding: BufferEncoding }>,
-        next: (param?: any) => void
+        next: any
     ): Promise<void> {
         const body = chunks
             .map((chunk) => chunk.chunk)
@@ -56,8 +64,7 @@ class ElasticsearchWritableStream extends stream.Writable {
         try {
             await this.client.bulk({
                 index: this.config.index,
-                type: this.config.type,
-                body: body.toString(),
+                body: body,
             });
             next();
         } catch (err) {
